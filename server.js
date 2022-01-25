b32a3d301cda4fcaa5d840f047999d13627fc5e2
Ouches Technology <36384023+hambolu@users.noreply.github.com>
2022-01-25 03:29:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ var erc20 = require('erc20-wallet');
 const Web3 = require('web3');
 var bodyParser = require('body-parser')
 const PORT = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`Listing ${port}`)
-})
+
 
 //let web3 = new Web3('ws://localhost:8546');
 
@@ -110,3 +108,6 @@ app.get('/bnbtx', function (req, res){
 
 //app.listen(3000, () => console.log('working'))
 
+app.listen(port, () => {
+  console.log(`Listing ${port}`)
+})
