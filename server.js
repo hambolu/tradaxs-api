d63545845f48763cab244ab59e1f77ff4877533e
Flesktech <ouchestechnology@gmail.com>
2022-04-18 10:45:39 +0100

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 var cw = require('crypto-wallets');
 const app = express();
 const path = require('path');
-var {engine}  = require('express-handlebars');
+var { engine } = require('express-handlebars');
 const blockexplorer = require('blockexplorer');
 var WAValidator = require('wallet-address-validator');
 // var balance = require('crypto-balances');
@@ -29,37 +29,37 @@ app.engine('.js', engine());
 app.set('views', path.join(__dirname, './apis'));
 app.set('view engine', '.js');
 
-app.get('/', function (req, res){      
-res.json('App is working');
+app.get('/', function(req, res) {
+    res.json('App is working');
 });
 
 
-app.get('/btc', function (req, res){      
-var bitcoinWallet = cw.generateWallet('BTC');
-res.json(bitcoinWallet);
+app.get('/btc', function(req, res) {
+    var bitcoinWallet = cw.generateWallet('BTC');
+    res.json(bitcoinWallet);
 });
 
-app.get('/eth', function (req, res){
+app.get('/eth', function(req, res) {
     var ethWallet = cw.generateWallet('ETH');
-res.json(ethWallet);
+    res.json(ethWallet);
 });
 
-app.get('/bch', function (req, res){
+app.get('/bch', function(req, res) {
     var bchWallet = cw.generateWallet('BCH');
-res.json(bchWallet);
+    res.json(bchWallet);
 });
 
-app.get('/ltc', function (req, res){
+app.get('/ltc', function(req, res) {
     var ltcWallet = cw.generateWallet('LTC');
-res.json(ltcWallet);
+    res.json(ltcWallet);
 });
 
-app.get('/doge', function (req, res){
+app.get('/doge', function(req, res) {
     var dogeWallet = cw.generateWallet('DOGE');
-res.json(dogeWallet);
+    res.json(dogeWallet);
 });
 
-app.get('/bnb', function (req, res){
+app.get('/bnb', function(req, res) {
     var bnb = web3.eth.accounts.create();
     res.json(bnb);
 });
@@ -77,15 +77,15 @@ var jsonParser = bodyParser.json()
 
 // });
 
-app.get('/balance', jsonParser,function (req, res){
+app.get('/balance', jsonParser, function(req, res) {
     var pk = req.body.pk;
-//     var cryptobalance = balance(pk, function(error, result) {
-//         res.json(result);
-//       });
-    
+    //     var cryptobalance = balance(pk, function(error, result) {
+    //         res.json(result);
+    //       });
+
     balance(pk)
-.then(result => res.json(result))
-.catch(error => res.json(`OH NO! ${error}`));
+        .then(result => res.json(result))
+        .catch(error => res.json(`OH NO! ${error}`));
 
 });
 
@@ -94,40 +94,40 @@ app.get('/balance', jsonParser,function (req, res){
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.post('/bnb-balance', jsonParser, function (req, res){
-     var balance = req.body.balance;
+app.post('/bnb-balance', jsonParser, function(req, res) {
+    var balance = req.body.balance;
     web3.eth.getBalance(balance, function(error, result) {
         res.json(result);
-      });
+    });
 });
 //req.body.balance
-app.get('/gas', function (req, res){
-   var gas = web3.eth.generate_gas_price();
+app.get('/gas', function(req, res) {
+    var gas = web3.eth.generate_gas_price();
     res.json(gas);
 });
-app.post('/bnbtx', jsonParser, function (req, res){
-    
+app.post('/bnbtx', jsonParser, function(req, res) {
+
     var decimals = 18;
-    var value = (req.body.amount*(10**decimals)).toString();
+    var value = (req.body.amount * (10 ** decimals)).toString();
     var amount = web3.utils.toBN(value);
-    
+
     var tx = {
-        to: req.body.holder,
-        value: amount,
-        gasPrice: req.body.gprice,
-        gas: req.body.gas,
-    }
-    // transactionhash = 0xc3b7e98d5d2b0858aa8045a6a8af5ce713897864bc85609dc53cfbdcaa3dfc17
+            to: req.body.holder,
+            value: amount,
+            gasPrice: req.body.gprice,
+            gas: req.body.gas,
+        }
+        // transactionhash = 0xc3b7e98d5d2b0858aa8045a6a8af5ce713897864bc85609dc53cfbdcaa3dfc17
     var privateKey = req.body.pkey;
 
     web3.eth.accounts.signTransaction(tx, privateKey)
         .then(signed => {
-        web3.eth.sendSignedTransaction(signed.rawTransaction)
-        .on('error',function(error){ res.status(404).json(error)})
-        .on('receipt',function(receipt){ res.status(200).json(receipt)})
-    });
+            web3.eth.sendSignedTransaction(signed.rawTransaction)
+                .on('error', function(error) { res.status(404).json(error) })
+                .on('receipt', function(receipt) { res.status(200).json(receipt) })
+        });
 
-    
+
 });
 
 //app.listen(3000, () => console.log('working'))
@@ -152,6 +152,25 @@ app.get('/bal', jsonParser, async(req, res, next) => {
     res.json({ "balance": balance });
 })
 
+app.get('/send', jsonParser, async(req, res, next) => {
+    var privKey = req.body.pKey;
+    var toAddress = req.body.toAddress;
+    var fromAddress = req.body.fromAddress;
+    var amount = req.body.amount;
+    const account = new CryptoAccount(privKey);
+
+
+    await account.send(toAddress, amount, {
+        type: "ERC20",
+        address: fromAddress,
+    }).then((response) => {
+        res.json(response);
+    }).catch((error) => {
+        res.json(error);
+    });
+
+})
+
 app.listen(PORT, () => {
-  console.log(`Listing ${PORT}`)
+    console.log(`Listing ${PORT}`)
 })
